1f74f82b660882d1023f978816d86d8a87d0e1da
Turtletnt5 <mdturtleman@gmail.com>
2024-05-18 15:22:32 -0400

    movment almost their

--- Characters/punk.gd
+++ Characters/punk.gd
@@ -7,14 +7,22 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var AnimFlip : bool = false
+var moving : bool = false
+
+@onready var MoveLine : Line2D = $Lines/MoveLine
+var MoveLineIndex : int = 0
+var delay : int = 0
+
+var targetPosition : Vector2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-@onready var anim = $AnimatedSprite2D
+@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 
 
+	 
 
 func _physics_process(delta):
 	
@@ -23,6 +31,19 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	
 
+	if moving and MoveLine.points.size() > MoveLineIndex+1:
+		
+		var direction = clampi( targetPosition.x - position.x, -1, 1)
+		#print_debug(direction)
+		velocity.x = direction * SPEED
+		
+		if (position.x - targetPosition.x) < 1 and (position.x - targetPosition.x) > -1:
+			MoveLineIndex += 1
+			calcutateTargetPosition()
+			
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 
 
 	# animations
@@ -33,7 +54,8 @@ func _physics_process(delta):
 		else :
 			AnimFlip = true
 		
-		anim.flip_v = AnimFlip
+		anim.flip_h = AnimFlip
+		
 		anim.play("Run")
 
 	if sqrt(velocity.y * velocity.y) > 5:
@@ -47,3 +69,14 @@ func _physics_process(delta):
 
 
 
+
+func startMoving():
+	#MoveLine.visible = false
+	MoveLineIndex = 0
+	calcutateTargetPosition()
+	moving = true
+
+	
+func calcutateTargetPosition():
+	
+	targetPosition = position + (MoveLine.points[MoveLineIndex+1] - MoveLine.points[MoveLineIndex])
